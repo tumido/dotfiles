6f20b9eaddd70f0b7ea84322e4ff4235f9c88ad5
Tomas Coufal <tcoufal@redhat.com>
2020-09-11 13:03:29 +0200

    Update repos and current session

--- dot_scripts/executable_git_clone_repositories.sh
+++ dot_scripts/executable_git_clone_repositories.sh
@@ -13,24 +13,6 @@ git clone git@github.com:tumido/continuous-deployment.git /home/tcoufal/Programm
 git --git-dir=/home/tcoufal/Programming/AI-CoE/operate-first/continuous-deployment/.git remote add upstream git@github.com:operate-first/continuous-deployment.git
 git --git-dir=/home/tcoufal/Programming/AI-CoE/operate-first/continuous-deployment/.git fetch --all
 
-# Repo: /home/tcoufal/Programming/AI-CoE/thoth-station/jupyternb-build-pipeline
-git clone git@github.com:tumido/jupyternb-build-pipeline.git /home/tcoufal/Programming/AI-CoE/thoth-station/jupyternb-build-pipeline -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/thoth-station/jupyternb-build-pipeline/.git remote add upstream git@github.com:thoth-station/jupyternb-build-pipeline.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/thoth-station/jupyternb-build-pipeline/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/thoth-station/micropipenv
-git clone git@github.com:tumido/micropipenv.git /home/tcoufal/Programming/AI-CoE/thoth-station/micropipenv -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/thoth-station/micropipenv/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/odh-ml-pipelines
-git clone git@github.com:tumido/odh-ml-pipelines.git /home/tcoufal/Programming/AI-CoE/odh-ml-pipelines -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/odh-ml-pipelines/.git remote add upstream git@github.com:anishasthana/odh-ml-pipelines.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/odh-ml-pipelines/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/s2i-custom-notebook
-git clone git@github.com:tumido/s2i-custom-notebook.git /home/tcoufal/Programming/AI-CoE/s2i-custom-notebook -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/s2i-custom-notebook/.git fetch --all
-
 # Repo: /home/tcoufal/Programming/AI-CoE/aiops/openshift-sme-topic-modeling
 git clone git@github.com:tumido/openshift-sme-mailing-list-analysis.git /home/tcoufal/Programming/AI-CoE/aiops/openshift-sme-topic-modeling -o origin
 git --git-dir=/home/tcoufal/Programming/AI-CoE/aiops/openshift-sme-topic-modeling/.git remote add upstream git@github.com:aicoe-aiops/openshift-sme-mailing-list-analysis.git
@@ -56,11 +38,6 @@ git clone git@github.com:tumido/aicoe-cd.git /home/tcoufal/Programming/AI-CoE/sr
 git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/aicoe-cd/.git remote add upstream git@github.com:AICoE/aicoe-cd.git
 git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/aicoe-cd/.git fetch --all
 
-# Repo: /home/tcoufal/Programming/AI-CoE/sre/aicoe-sre
-git clone git@github.com:tumido/aicoe-sre.git /home/tcoufal/Programming/AI-CoE/sre/aicoe-sre -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/aicoe-sre/.git remote add upstream git@github.com:AICoE/aicoe-sre.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/aicoe-sre/.git fetch --all
-
 # Repo: /home/tcoufal/Programming/AI-CoE/sre/aicoe-ci
 git clone git@github.com:AICoE/aicoe-ci.git /home/tcoufal/Programming/AI-CoE/sre/aicoe-ci -o upstream
 git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/aicoe-ci/.git fetch --all
@@ -71,46 +48,9 @@ git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/odh-manifests/.git remote add
 git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/odh-manifests/.git remote add upstream git@github.com:opendatahub-io/odh-manifests.git
 git --git-dir=/home/tcoufal/Programming/AI-CoE/sre/odh-manifests/.git fetch --all
 
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/dh-argo-workflows
-git clone git@gitlab.cee.redhat.com:tcoufal/dh-argo-workflows.git /home/tcoufal/Programming/AI-CoE/gitlab/dh-argo-workflows -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-argo-workflows/.git remote add upstream git@gitlab.cee.redhat.com:data-hub/dh-argo-workflows.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-argo-workflows/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/dh-internal-odh-install
-git clone git@gitlab.cee.redhat.com:data-hub/dh-internal-odh-install.git /home/tcoufal/Programming/AI-CoE/gitlab/dh-internal-odh-install -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-internal-odh-install/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/dh-hive
-git clone git@gitlab.cee.redhat.com:tcoufal/dh-hive.git /home/tcoufal/Programming/AI-CoE/gitlab/dh-hive -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-hive/.git remote add upstream git@gitlab.cee.redhat.com:data-hub/dh-hive.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-hive/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/reading-sos-reports
-git clone git@gitlab.cee.redhat.com:avsrivas/reading-sos-reports.git /home/tcoufal/Programming/AI-CoE/gitlab/reading-sos-reports -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/reading-sos-reports/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/dh-argo
-git clone git@gitlab.cee.redhat.com:tcoufal/dh-argo.git /home/tcoufal/Programming/AI-CoE/gitlab/dh-argo -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-argo/.git remote add upstream git@gitlab.cee.redhat.com:data-hub/dh-argo.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dh-argo/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/AI-CoE/gitlab/dev-ocp-provisioner
-git clone git@gitlab.cee.redhat.com:tcoufal/dev-ocp-provisioner.git /home/tcoufal/Programming/AI-CoE/gitlab/dev-ocp-provisioner -o origin
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dev-ocp-provisioner/.git remote add upstream git@gitlab.cee.redhat.com:data-hub/dev-ocp-provisioner.git
-git --git-dir=/home/tcoufal/Programming/AI-CoE/gitlab/dev-ocp-provisioner/.git fetch --all
-
 # Repo: /home/tcoufal/Programming/AI-CoE/yaml_filter
 git clone git@github.com:Agilicus/yaml_filter.git /home/tcoufal/Programming/AI-CoE/yaml_filter -o origin
 git --git-dir=/home/tcoufal/Programming/AI-CoE/yaml_filter/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/rambox
-git clone git@github.com:tumido/rambox.git /home/tcoufal/Programming/rambox -o origin
-git --git-dir=/home/tcoufal/Programming/rambox/.git remote add rambox-upstream git@github.com:ramboxapp/community-edition.git
-git --git-dir=/home/tcoufal/Programming/rambox/.git remote add upstream git@github.com:TheGoddessInari/hamsket.git
-git --git-dir=/home/tcoufal/Programming/rambox/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/kwin-tabbox-icons-only
-git --git-dir=/home/tcoufal/Programming/kwin-tabbox-icons-only/.git fetch --all
 
 # Repo: /home/tcoufal/Programming/malenovska
 git clone git@github.com:tumido/malenovska.git /home/tcoufal/Programming/malenovska -o origin
@@ -121,10 +61,6 @@ git clone git@github.com:tumido/ethel-old.git /home/tcoufal/Programming/Ethel -o
 git --git-dir=/home/tcoufal/Programming/Ethel/.git remote add origin git@github.com:tumido/ethel.git
 git --git-dir=/home/tcoufal/Programming/Ethel/.git fetch --all
 
-# Repo: /home/tcoufal/Programming/kwin-script-ultrawide-windows
-git clone git@github.com:LucaMoschella/UltrawideWindows.git /home/tcoufal/Programming/kwin-script-ultrawide-windows -o origin
-git --git-dir=/home/tcoufal/Programming/kwin-script-ultrawide-windows/.git fetch --all
-
 # Repo: /home/tcoufal/Programming/linux.pictures
 git clone https://gitlab.com/jstpcs/lnxpcs.git /home/tcoufal/Programming/linux.pictures -o origin
 git --git-dir=/home/tcoufal/Programming/linux.pictures/.git fetch --all
@@ -132,8 +68,3 @@ git --git-dir=/home/tcoufal/Programming/linux.pictures/.git fetch --all
 # Repo: /home/tcoufal/Programming/imgur-album-downloader
 git clone https://github.com/alexgisby/imgur-album-downloader.git /home/tcoufal/Programming/imgur-album-downloader -o origin
 git --git-dir=/home/tcoufal/Programming/imgur-album-downloader/.git fetch --all
-
-# Repo: /home/tcoufal/Programming/map-the-ldap
-git clone git@github.com:tumido/map-the-ldap.git /home/tcoufal/Programming/map-the-ldap -o origin
-git --git-dir=/home/tcoufal/Programming/map-the-ldap/.git fetch --all
-
